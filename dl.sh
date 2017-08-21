e1fa6fa8923f003a1ea4f12d3eb5b6d9abc75822
Ariel Kanterewicz <ariel@aknt.com.ar>
2017-08-21 14:36:21 -0300

    added chapter 73

--- dl.sh
+++ dl.sh
@@ -2,5 +2,5 @@
 mkdir -p dl/arc{1,2,3}
 curl 'https://www.fictionpress.com/s/2961893/[1-26]/Mother-of-Learning'  -o 'dl/arc1/#1.html'
 curl 'https://www.fictionpress.com/s/2961893/[27-54]/Mother-of-Learning' -o 'dl/arc2/#1.html'
-curl 'https://www.fictionpress.com/s/2961893/[55-72]/Mother-of-Learning' -o 'dl/arc3/#1.html'
+curl 'https://www.fictionpress.com/s/2961893/[55-73]/Mother-of-Learning' -o 'dl/arc3/#1.html'
 
